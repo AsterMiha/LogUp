dc313e540b359800849501c922a3efe0d7ec6eb4
AsterMiha <chireamihaela99@gmail.com>
2023-11-18 15:08:22 +0100

    Add test route

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request, jsonify
+import json
 
 app = Flask(__name__)
 
@@ -6,3 +8,19 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+# Receive data and echo it back
+# Reply format: {
+#	'hello': string
+#	'data': json received
+#	}
+@app.route('/echo', methods=["POST"])
+def echo():
+	data = json.loads(request.data)
+	print(data)
+	return jsonify({
+        'hello':'Hello!', 
+        'data':data,
+        })
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
